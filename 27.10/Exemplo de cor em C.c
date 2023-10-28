b3a440eb3704df217c7f73caf966d7bc5ee8704d
Tiago Leão <136996944+TigasLeao@users.noreply.github.com>
2023-10-27 21:25:32 -0300

    Create Exemplo de cor em C.c
    Exemplo de cores e posicionamento em texto.

---
+++ 27.10/Exemplo de cor em C.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main()
+{
+    printf("\x1B[44m");
+    printf("\x1B[97m");
+    printf("\x1B[12;30H");
+    printf("Como essa frase sera apresentada na tela?");
+    printf("\x1B[0m");
+
+    return 0;
+}
